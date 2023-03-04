30a09ee5fd81d39813d873c51b17382e68468090
deep0072 <deepakumar50136@gmail.com>
2023-03-05 01:35:31 +0530

    here we are  stopping event proagation to the parent div

--- src/components/eventHandlerAsProps/event.js
+++ src/components/eventHandlerAsProps/event.js
@@ -1,10 +1,29 @@
-function Button({ onClick, children }) {
+function Button({ onSmash, children }) {
   return (
     // onClick trigger  the handleCLick function then alert will be execute
     // children will render the content between Button jsx tag whihch is play "{movieName}"
-    <button onClick={onClick}>
+    <button
+      onClick={(e) => {
+        e.stopPropagation();
+        onSmash();
+      }}
+    >
       <div>{children}</div>
     </button>
+  );
+}
+
+function UploadButton() {
+  return (
+    <div>
+      <Button
+        onSmash={() => {
+          alert("image uploading");
+        }}
+      >
+        upload image
+      </Button>
+    </div>
   );
 }
 
@@ -13,14 +32,15 @@ export function PlayButton({ movieName }) {
     alert(`playing ${movieName}`);
   }
   return (
-    <div>
+    <div onClick={() => alert("oh hi there what is happening")}>
       <h1>
         ------------example of pasing eventHandlers as props given
         below----------------------
       </h1>
       <br></br>
       {/* here we are passing "handleClick" eventHandler  as a prop */}
-      <Button onClick={handleClick}>play "{movieName}"</Button>
+      <Button onSmash={handleClick}>play "{movieName}"</Button>
+      <UploadButton />
     </div>
   );
 }
