e029f42ca46414d38a9a37a5e7dc5e5acb660a9b
deep0072 <deepakumar50136@gmail.com>
2024-05-07 15:15:44 +0530

    add sign up route

--- week8Tailwind/paytm/backend/routes/api/user.js
+++ week8Tailwind/paytm/backend/routes/api/user.js
@@ -1,3 +1,44 @@
 const express = require("express")
+const {User} = require("../../db")
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+const zod = require("zod") //  acts very similarly to serializers in Django REST 
+//Framework during deserialization, providing data validation for incoming requests
 const router = express.Router()
+const signUpSchema = zod.object({
+    username:zod.string(),
+    password : zod.string(), // these are like serializer field for incoming request
+    firstName:zod.string(),
+    lastName:zod.string()
+})
+router.post("/signup" ,async (res,req)=>{
+    const body = req.body
+    const success = signUpSchema.safeParse(body)
+    if (!success ){
+        return res.status(411).json({
+            message:"INCORRECT INPUTS"
+        })
+    }
+
+    const user = await User.findOne({username:body.username})
+    if (user._id){
+        return res.status(411).json({
+            message:"USER ALREADY EXISTS"
+        })
+    }
+    const dbUser = await User.create(body)
+    const userId = dbUser._id;
+    const token = jwt.sign({
+        userId
+
+
+    }, JWT_SECRET)
+    res.json({
+        message:"User created successfully",
+        token:token
+    })
+
+
+
+})
 module.exports = router
